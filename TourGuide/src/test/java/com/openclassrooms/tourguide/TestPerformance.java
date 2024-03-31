c9dc5fec9bb8cbbad701dac483665d5efebc51cd
Franck Gimenez <fk.gimenez@gmail.com>
2024-03-31 16:54:49 +0000

    Update TestPerformance.java

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -54,13 +54,14 @@ public class TestPerformance {
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100000);
+		InternalTestHelper.setInternalUserNumber(1000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService, rewardCentral);
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 
-		tourGuideService.trackUserLocations(tourGuideService.getAllUsers());
+		//tourGuideService.trackUserLocations(tourGuideService.getAllUsers());
+		allUsers.parallelStream().forEach(tourGuideService::trackUserLocation);
 
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
@@ -105,7 +106,7 @@ public class TestPerformance {
 
 		// Users should be incremented up to 100,000, and test finishes within 20
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100000);
+		InternalTestHelper.setInternalUserNumber(1000);
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService, rewardCentral);
@@ -115,7 +116,8 @@ public class TestPerformance {
 		allUsers = tourGuideService.getAllUsers();
 		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
 
-		rewardsService.calculateRewardsForAllUsers(allUsers);
+		//rewardsService.calculateRewardsForAllUsers(allUsers);
+		allUsers.parallelStream().forEach(rewardsService::calculateRewards);
 
 		for (User user : allUsers) {
 			assertTrue(user.getUserRewards().size() > 0);
